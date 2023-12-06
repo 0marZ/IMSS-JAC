6028797c9e65259fa77e8f4769ca287b2ee1a734
0marZ <omarzarate.ozm@gmail.com>
2023-12-06 11:08:21 -0600

    Botones paginacion en index Cer.P

--- Cer_P_Mod40/index.php
+++ Cer_P_Mod40/index.php
@@ -1,127 +1,160 @@
 <?php include('connection.php'); ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="es">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Bootstrap CSS -->
-  <link href="css/bootstrap5.0.1.min.css" rel="stylesheet" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="css/datatables-1.10.25.min.css" />
-  <link rel="icon" type="image/png" href="img/fevicon.png">
-  <!-- Archivo CSS  -->
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <title>CERTIFICACIÓN DE DATOS DE PAGO IMSS</title>
-
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap5.0.1.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
+    <script src="https://kit.fontawesome.com/db489652f4.js" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/png" href="img/fevicon.png">
+    <!-- Archivo CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>CERTIFICACIÓN DE DATOS DE PAGO IMSS</title>
 </head>
-<nav class="navbar navbar-expand-lg navbar-dark ">
-  <div class="container-fluid">
-    <!-- Logo del IMSS -->
-    <a class="navbar-brand" href="../">
-      <img src="img/JAC_Logo3.png" alt="IMSS Logo" width="240" height="90" class="d-inline-block align-top">
-    </a>
-    <!-- Logo del Gobierno de México 
-    <a class="navbar-brand" href="#">
-      <img src="img/logoheader.svg" alt="Gobierno de México Logo" width="180" height="65" class="d-inline-block align-top">
-    </a>
 
--->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">MODALIDAD 40</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../">HOME</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            OTROS
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">1. </a></li>
-            <li><a class="dropdown-item" href="#">2. </a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">3. </a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-      </div>
-</nav>
-<body>
-  <div class="container-fluid mt-4">
-    <h2 class="text-center">CERTIFICACIÓN DE DATOS DE PAGO IMSS</h2>
-    <p class="datatable design text-center">MODALIDAD 40</p>
-    <div class="row">
-      <div class="container">
-
-        <div class="row">
-          <div class="col-md-1"></div>
-          <div class="col-md-10">
-            <table id="example" class="table">
-              <thead>
-                <th>Id</th>
-                <th>Cve Delegacion</th>
-                <th>Cve Nss</th>
-                <th>Cve Sub Delegacion</th>
-                <th>Fecha Pago</th>
-                <th>Opciones</th>
-              </thead>
-              <tbody>
-              </tbody>
-            </table>
-          </div>
-          <div class="col-md-1"></div>
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid">
+        <!-- Logo del IMSS -->
+        <a class="navbar-brand" href="../">
+            <img src="img/JAC_Logo3.png" alt="IMSS Logo" width="240" height="90" class="d-inline-block align-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">MODALIDAD 40</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../">HOME</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        OTROS
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">1. </a></li>
+                        <li><a class="dropdown-item" href="#">2. </a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">3. </a></li>
+                    </ul>
+                </li>
+            </ul>
         </div>
-      </div>
     </div>
-  </div>
-  <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="js/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
-  <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-  <script type="text/javascript" src="js/dt-1.10.25datatables.min.js"></script>
-  <script type="text/javascript">
-    $(document).ready(function() {
-      $('#example').DataTable({
+</nav>
+
+<body>
+    <div class="container-fluid mt-4">
+        <h2 class="text-center">CERTIFICACIÓN DE DATOS DE PAGO IMSS</h2>
+        <p class="datatable design text-center">MODALIDAD 40</p>
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10">
+                        <table id="example" class="table">
+                            <thead>
+                                <th>Id</th>
+                                <th>Cve Delegacion</th>
+                                <th>Cve Nss</th>
+                                <th>Cve Sub Delegacion</th>
+                                <th>Fecha Pago</th>
+                                <th>Opciones</th>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-md-1"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>
+
+    <script type="text/javascript">
+$(document).ready(function () {
+    $('#example').DataTable({
         "language": {
-            "processing":"Procesando información.....", 
-            "lengthMenu": "Mostrar _MENU_ registros por pagina",
+            "processing": "Procesando información.....",
+            "lengthMenu": "Mostrar _MENU_ registros por página",
             "zeroRecords": "No hay coincidencias",
             "info": "Mostrando la _PAGE_ de _PAGES_",
             "infoEmpty": "No records available",
             "infoFiltered": "(Filtrado de _MAX_ registros totales)",
             "search": "Buscar:",
-            "paginate":{
-              "next": "Siguiente",
-              "previous":"Anterior"
+            "paginate": {
+                "next": "Siguiente",
+                "previous": "Anterior"
             }
-            
         },
-        "fnCreatedRow": function(nRow, aData, iDataIndex) {
-          $(nRow).attr('id', aData[0]);
+        "fnCreatedRow": function (nRow, aData, iDataIndex) {
+            $(nRow).attr('id', aData[0]);
         },
         'serverSide': 'true',
         'processing': 'true',
         'paging': 'true',
         'order': [],
         'ajax': {
-          'url': 'fetch_data.php',
-          'type': 'post',
+            'url': 'fetch_data.php',
+            'type': 'post',
         },
         "aoColumnDefs": [{
             "bSortable": false,
             "aTargets": [5]
-          },
-
+        }],
+        "dom": '<"top" lf>tBipr',
+        "lengthMenu": [10, 25, 50, 100],
+        "buttons": [
+            {
+                extend: 'copy',
+                text: '<i class="fas fa-copy"></i> Copiar',
+                className: 'btn btn-secondary'
+            },
+            {
+                extend: 'csv',
+                text: '<i class="fas fa-file-csv"></i> CSV',
+                className: 'btn btn-secondary'
+            },
+            {
+                extend: 'excel',
+                text: '<i class="fas fa-file-excel"></i> Excel',
+                className: 'btn btn-secondary'
+            },
+            {
+                extend: 'pdf',
+                text: '<i class="fas fa-file-pdf"></i> PDF',
+                className: 'btn btn-secondary'
+            },
+            {
+                extend: 'print',
+                text: '<i class="fas fa-print"></i> Imprimir',
+                className: 'btn btn-secondary'
+            }
         ]
-      });
     });
+});
+
+
     $(document).on('submit', '#addUser', function(e) {
       e.preventDefault();
       var city = $('#addCityField').val();
