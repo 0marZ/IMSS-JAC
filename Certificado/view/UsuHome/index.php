f1f386ff692ceafa46a8a976e60bd24cb09caa05
0marZ <omarzarate.ozm@gmail.com>
2024-03-01 10:15:55 -0600

    Optimizacion de codigo en Index Usu Home

--- Certificado/view/UsuHome/index.php
+++ Certificado/view/UsuHome/index.php
@@ -71,7 +71,7 @@
                 <div class="d-flex align-items-center">
                   <i class="ion-android-laptop tx-60 lh-0 tx-warning op-7"></i>
                   <div class="mg-l-20">
-                    <p class="tx-10 tx-spacing-1 tx-mont tx-medium tx-uppercase tx-gray-600 mg-b-10">Total de trabajadores PTD</p>
+                    <p class="tx-10 tx-spacing-1 tx-mont tx-medium tx-uppercase tx-gray-600 mg-b-10">Trabajadores PTD</p>
                     <p class="tx-24 tx-gray-800 tx-lato tx-bold mg-b-2 lh-1" id="lblotrainfo">0</p> 
                   </div>
                 </div>
@@ -147,10 +147,7 @@
   <?php
     }else{
   ?>
-
-  
-
-      <!-- Datos para la vista usuario (FALTA DEFINIR QUE DATOS SE NESECITAN MOSTRAR-->
+  <!-- Datos para la vista usuario (FALTA DEFINIR QUE DATOS SE NESECITAN MOSTRAR-->
   <div class="br-mainpanel">
         <div class="br-pageheader pd-y-15 pd-l-20">
           <nav class="breadcrumb pd-0 mg-0 tx-12">
@@ -162,7 +159,6 @@
           <p class="mg-b-0">Dashboard</p>
         </div>
 
-
       <!-- Contenido del proyecto -->
       <div class="br-pagebody mg-t-5 pd-x-30">
           <!-- Contenedor blanco con cuadros de información -->
@@ -171,17 +167,15 @@
             <div class="col-md-6 col-xl-5">
                 <div class="card">
                     <div class="bg-white rounded overflow-hidden pd-85">
-                    <div class="d-flex align-items-center">
-                        
-                            <!-- Contenedor para el reloj digital -->
-                            <div id="digital-clock-container" style="position: absolute; left: 25px;">
-                                <!-- Agregar la imagen del icono de reloj -->
-                                <img src="../../2784399.png" alt="Reloj" style="width: 90px; margin-right: 20px;">
-                                <!-- Agregar el reloj digital -->
-                                <div id="digital-clock" style="font-size: 50px; display: inline-block;"></div>
-                            </div>
-                    </div>
-                        
+                      <div class="d-flex align-items-center">
+                          <!-- Contenedor para el reloj digital -->
+                          <div id="digital-clock-container" style="position: absolute; left: 25px;">
+                              <!-- Agregar la imagen del icono de reloj -->
+                              <img src="../../2784399.png" alt="Reloj" style="width: 90px; margin-right: 20px;">
+                              <!-- Agregar el reloj digital -->
+                              <div id="digital-clock" style="font-size: 50px; display: inline-block;"></div>
+                          </div>
+                      </div>
                     </div>
                 </div>
             </div>
@@ -214,13 +208,13 @@
               </div>
             </div>
         </div>
-
       </div>
   </div>
 
   <?php
       }
     ?>
+
   <script>
         // Función para actualizar el reloj digital
         function updateClock() {
@@ -248,112 +242,99 @@
         updateClock();
     </script>
     <!-- Script para cargar los datos y generar la gráfica -->
-      <script>
+    <script>
         
-        const data = {
-            labels: ['Orizaba', 'Cordoba', 'Cozamaloapan', 'Coatzacoalcos'],
-            datasets: [{
-                label: 'Trabajadores PTD por ciudad',
-                data: [31, 39, 17, 54],
-                backgroundColor: 'rgba(38, 102, 87, 0.5)',
-                borderColor: 'rgba(19, 50, 43, 1)',
-                borderWidth: 1
-            }]
-        };
-
-        // Configurar opciones de la gráfica
-        const options = {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        };
-
-        // Crear la gráfica de barras
-        var ctx = document.getElementById('barChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: data,
-            options: options
-        });
-
-        // Crear el mapa con Leaflet.js
-        var map = L.map('mapid').setView([18.932748, -95.620954], 7);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright"target="_blank">OpenStreetMap</a>'
-        }).addTo(map);
-        L.control.scale().addTo(map);
-        // Define las coordenadas de las ciudades y sus datos
-        var cities = [
-             { name: 'ORIZABA', location: [18.847440, -97.087738], data: 31 },
-            { name: 'COATZACOALCOS', location: [18.133281, -94.466169], data: 54 },
-            { name: 'CORDOBA', location: [18.886151, -96.931693], data: 39 },
-            { name: 'COSAMALOAPAN', location: [18.366228, -95.782712], data: 17 }
-        ];
-
-        // Inicializar el índice de la ciudad actual
-        var currentIndex = 0;
-
-        // Función para agregar un marcador al mapa y mostrar su información
-        function addMarker() {
-            // Verificar si ya se mostraron todos los marcadores
-            if (currentIndex >= cities.length) {
-                // Detener el intervalo
-                clearInterval(intervalId);
-                return; // Salir de la función
-            }
-            
-            // Obtener la ciudad actual
-            var city = cities[currentIndex];
-            
-            // Construir el contenido del popup
-            var popupContent = "<b>" + city.name + "</b> <br/>" +
-                              "Datos: " + city.data;
-
-            // Crear un marcador para la ciudad actual
-            var marker = L.marker(city.location).addTo(map);
-            
-            // Asociar un popup con el contenido generado a cada marcador
-            marker.bindPopup(popupContent);
-            // Mostrar el popup del primer marcador
-            marker.openPopup();
-            
-            // Incrementar el índice para pasar a la siguiente ciudad en el siguiente intervalo
-            currentIndex++;
-        }
-
-        // Definir el intervalo de tiempo entre la adición de cada marcador (en milisegundos)
-        var interval = 3000; // 3 segundos
-
-        // Agregar el primer marcador inmediatamente
-        addMarker();
-
-        // Configurar el temporizador para agregar los marcadores automáticamente
-        var intervalId = setInterval(addMarker, interval);
-
-        // Función para reiniciar el ciclo mostrando el primer marcador nuevamente
-          function restartMarkers() {
-              // Reiniciar el índice
-              currentIndex = 0;
-              // Limpiar el mapa de todos los marcadores actuales
-              map.eachLayer(function(layer) {
-                  if (layer instanceof L.Marker) {
-                      map.removeLayer(layer);
-                  }
-              });
-              // Reiniciar el intervalo para agregar los marcadores automáticamente
-              clearInterval(intervalId);
-              intervalId = setInterval(addMarker, interval);
-          }
-
-          // Configurar el temporizador para reiniciar el ciclo después de un cierto tiempo
-          var restartInterval = 25000; // 25 segundos
-          setInterval(restartMarkers, restartInterval);
+       const data = {
+           labels: ['Orizaba', 'Cordoba', 'Cozamaloapan', 'Coatzacoalcos'],
+           datasets: [{
+               label: 'Trabajadores PTD por ciudad',
+               data: [31, 39, 17, 54],
+               backgroundColor: 'rgba(38, 102, 87, 0.5)',
+               borderColor: 'rgba(19, 50, 43, 1)',
+               borderWidth: 1
+           }]
+       };
+       // Configurar opciones de la gráfica
+       const options = {
+           scales: {
+               y: {
+                   beginAtZero: true
+               }
+           }
+       };
+       // Crear la gráfica de barras
+       var ctx = document.getElementById('barChart').getContext('2d');
+       var myChart = new Chart(ctx, {
+           type: 'bar',
+           data: data,
+           options: options
+       });
+       // Crear el mapa con Leaflet.js
+       var map = L.map('mapid').setView([18.932748, -95.620954], 7);
+       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright"target="_blank">OpenStreetMap</a>'
+       }).addTo(map);
+       L.control.scale().addTo(map);
+       // Define las coordenadas de las ciudades y sus datos
+       var cities = [
+            { name: 'ORIZABA', location: [18.847440, -97.087738], data: 31 },
+           { name: 'COATZACOALCOS', location: [18.133281, -94.466169], data: 54 },
+           { name: 'CORDOBA', location: [18.886151, -96.931693], data: 39 },
+           { name: 'COSAMALOAPAN', location: [18.366228, -95.782712], data: 17 }
+       ];
+       // Inicializar el índice de la ciudad actual
+       var currentIndex = 0;
+       // Función para agregar un marcador al mapa y mostrar su información
+       function addMarker() {
+           // Verificar si ya se mostraron todos los marcadores
+           if (currentIndex >= cities.length) {
+               // Detener el intervalo
+               clearInterval(intervalId);
+               return; // Salir de la función
+           }
+           
+           // Obtener la ciudad actual
+           var city = cities[currentIndex];
+           
+           // Construir el contenido del popup
+           var popupContent = "<b>" + city.name + "</b> <br/>" +
+                             "Datos: " + city.data;
+           // Crear un marcador para la ciudad actual
+           var marker = L.marker(city.location).addTo(map);
+           
+           // Asociar un popup con el contenido generado a cada marcador
+           marker.bindPopup(popupContent);
+           // Mostrar el popup del primer marcador
+           marker.openPopup();
+           
+           // Incrementar el índice para pasar a la siguiente ciudad en el siguiente intervalo
+           currentIndex++;
+       }
+           // Definir el intervalo de tiempo entre la adición de cada marcador (en milisegundos)
+           var interval = 3000; // 3 segundos
+           // Agregar el primer marcador inmediatamente
+           addMarker();
+           // Configurar el temporizador para agregar los marcadores automáticamente
+           var intervalId = setInterval(addMarker, interval);
+           // Función para reiniciar el ciclo mostrando el primer marcador nuevamente
+           function restartMarkers() {
+             // Reiniciar el índice
+             currentIndex = 0;
+             // Limpiar el mapa de todos los marcadores actuales
+             map.eachLayer(function(layer) {
+                 if (layer instanceof L.Marker) {
+                     map.removeLayer(layer);
+                 }
+             });
+             // Reiniciar el intervalo para agregar los marcadores automáticamente
+             clearInterval(intervalId);
+             intervalId = setInterval(addMarker, interval);
+         }
+         // Configurar el temporizador para reiniciar el ciclo después de un cierto tiempo
+         var restartInterval = 25000; // 25 segundos
+         setInterval(restartMarkers, restartInterval);
 
     </script>
-
-
 
     <script type="text/javascript">
     
@@ -363,51 +344,43 @@
 
         function drawChart() {
 
-            var data = google.visualization.arrayToDataTable([
-                ['Label', 'Value'],
-                ['', 20],
-                ['', 45],
-            ]);
-
-            var options = {
-                width: 260,
-                height: 260,
-                redFrom: 90,
-                redTo: 100, // Rango rojo (90-100)
-                yellowFrom: 75,
-                yellowTo: 90, // Rango amarillo (75-90)
-                greenFrom: 0, // Rango verde (0-75)
-                greenTo: 75,
-                minorTicks: 8,
-            };
-
-            var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
-
-            chart.draw(data, options);
-
-            // Agregar texto personalizado abajo de cada indicador
-            var chartDiv = document.getElementById('chart_div');
-
-            var incidenciasText = document.createElement('p');
-            incidenciasText.innerHTML = 'Incidencias';
-            incidenciasText.style.position = 'absolute';
-            incidenciasText.style.bottom = '-20px'; // Ajusta según tu preferencia
-            incidenciasText.style.left = '50%';
-            incidenciasText.style.transform = 'translateX(55%)'; // Centrar horizontalmente
-            chartDiv.appendChild(incidenciasText);
-
-            var asistenciasText = document.createElement('p');
-            asistenciasText.innerHTML = ' Retardos ';
-            asistenciasText.style.position = 'absolute';
-            asistenciasText.style.bottom = '-20px'; // Ajusta según tu preferencia
-            asistenciasText.style.left = '50%';
-            asistenciasText.style.transform = 'translateX(-125%)'; // Centrar horizontalmente
-            chartDiv.appendChild(asistenciasText);
-
-            setInterval(function() {
-                data.setValue(0, 1, 40 + Math.round(50 * Math.random()));
-                data.setValue(1, 1, 40 + Math.round(50 * Math.random()));
-                chart.draw(data, options);
+          var data = google.visualization.arrayToDataTable([
+              ['Label', 'Value'],
+              ['', 20],
+              ['', 45],
+          ]);
+          var options = {
+              width: 260,
+              height: 260,
+              redFrom: 90,
+              redTo: 100, // Rango rojo (90-100)
+              yellowFrom: 75,
+              yellowTo: 90, // Rango amarillo (75-90)
+              greenFrom: 0, // Rango verde (0-75)
+              greenTo: 75,
+              minorTicks: 8,
+          };
+          var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+          chart.draw(data, options);
+          var chartDiv = document.getElementById('chart_div');
+          var incidenciasText = document.createElement('p');
+          incidenciasText.innerHTML = 'Incidencias';
+          incidenciasText.style.position = 'absolute';
+          incidenciasText.style.bottom = '-20px';
+          incidenciasText.style.left = '50%';
+          incidenciasText.style.transform = 'translateX(55%)';
+          chartDiv.appendChild(incidenciasText);
+          var asistenciasText = document.createElement('p');
+          asistenciasText.innerHTML = ' Retardos ';
+          asistenciasText.style.position = 'absolute';
+          asistenciasText.style.bottom = '-20px';
+          asistenciasText.style.left = '50%';
+          asistenciasText.style.transform = 'translateX(-125%)';
+          chartDiv.appendChild(asistenciasText);
+          setInterval(function() {
+              data.setValue(0, 1, 40 + Math.round(50 * Math.random()));
+              data.setValue(1, 1, 40 + Math.round(50 * Math.random()));
+              chart.draw(data, options);
             }, 13000);
         }
     </script>
